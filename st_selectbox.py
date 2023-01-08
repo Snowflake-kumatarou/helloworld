36231f17ffe97ac38ab71cf5f0314bb8c71da1ec
Snowflake-kumatarou <121083706+Snowflake-kumatarou@users.noreply.github.com>
2023-01-09 05:57:08 +0900

    Update st_selectbox.py

--- st_selectbox.py
+++ st_selectbox.py
@@ -4,6 +4,6 @@ st.header('st.selectbox')
 
 option = st.selectbox(
      'What is your favorite color?',
-     ('Blue', 'Red', 'Green'))
+     ('Snowflake Blue', 'Mid Blue', 'Midnight'))
 
 st.write('Your favorite color is ', option)
